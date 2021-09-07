67a517e773c6c429a8a9cd4e709ccec9388b620f
Vinay288 <vinayhiremath288@gmail.com>
2021-09-07 23:43:41 +0530

    ADDED: regex expression to validate lastname

--- src/com/bridgelabz/userregistrationproblem/UserRegistration.java
+++ src/com/bridgelabz/userregistrationproblem/UserRegistration.java
@@ -9,13 +9,19 @@ public class UserRegistration {
 		System.out.println("enter first name");
 		Scanner scanner = new Scanner(System.in);
 		String firstName = scanner.next();
-		if (validate(firstName))
+		if (validateName(firstName))
 			System.out.println("valid firstName");
 		else
 			System.out.println("inavlid first name");
+		System.out.println("enter last name");
+		String lastName = scanner.next();
+		if (validateName(lastName))
+			System.out.println("valid lastName");
+		else
+			System.out.println("inavlid lastt name");
 	}
 
-	private static boolean validate(String firstName) {
+	private static boolean validateName(String firstName) {
 		String nameValidation = "^[A-Z][A-Za-z]{2,}$";
 		return Pattern.matches(nameValidation, firstName);
 	}
