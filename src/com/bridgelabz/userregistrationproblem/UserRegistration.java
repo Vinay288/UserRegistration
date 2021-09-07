b72641017e1f15ac1fe864227f018807e18df3a4
Vinay288 <vinayhiremath288@gmail.com>
2021-09-08 00:10:23 +0530

    ADDED: regex expression to validate password with atleast 1 number

--- src/com/bridgelabz/userregistrationproblem/UserRegistration.java
+++ src/com/bridgelabz/userregistrationproblem/UserRegistration.java
@@ -44,7 +44,7 @@ public class UserRegistration {
 	}
 
 	private static boolean passwordValidation(String password) {
-		String passwordValidation = "(?=.*[A-Z])[a-zA-Z]{8,}";
+		String passwordValidation = "(?=.*[0-9])(?=.*[A-Z])[a-zA-Z]{8,}";
 		return Pattern.matches(passwordValidation, password);
 	}
 
